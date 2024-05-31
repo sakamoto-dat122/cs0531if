82ad72601ed9572021d7f686733e110934c145f5
haruto nakamura <104044075+noah-zty@users.noreply.github.com>
2024-05-31 15:45:07 +0900

    反転

--- cs0531if/Form1.cs
+++ cs0531if/Form1.cs
@@ -14,6 +14,21 @@ namespace cs0531if
         {
             label1.Left += vx;
             label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = -vx;
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = -vy;
+            }
+
+            if (label1.Left > 800)
+            {
+                vx = -vx;
+            }
         }
     }
 }
